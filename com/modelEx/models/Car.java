9ff0db018d8386cea65f1ae0949371ddd9d69cdc
pkorrie <patiencekorrie@gmail.com>
2021-10-15 15:19:43 -0400

    added models folder

---
+++ com/modelEx/models/Car.java
@@ -0,0 +1,15 @@
+package com.modelEx.models;
+
+ public class Car{
+     
+    public String Color;
+    public int model;
+    public String name;
+    public boolean isLoud;
+ 
+     public Car(){}
+ 
+     public Car(String name){
+         this.name = name;
+     }
+    }
---
+++ com/modelEx/toyota/Toyota.java
@@ -0,0 +1,10 @@
+package com.modelEx;
+import com.modelEx.models.Car;
+
+public class Toyota{
+
+    public static void main(String[] args){
+        Car Dimples = new Car("Dimples");
+        System.out.println("My dog's name is: " + Dimples);
+    }
+}
