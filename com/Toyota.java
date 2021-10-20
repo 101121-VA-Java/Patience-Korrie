48b0cdb4de0a33bbf5d88ce48dc3528bf54eaea6
pkorrie <patiencekorrie@gmail.com>
2021-10-20 14:25:44 -0400

    added BoringTask class

--- com/Toyota.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Toyota{
-
-    public static void main(String[] args){
-        Car Dimples = new Car("Dimples");
-        System.out.println("My car's name is: " + Dimples.name);
-    }
-}
---
+++ oop-demo/src/main/java/com/revature/models/BoringTask.java
@@ -0,0 +1,22 @@
+package com.revature.models;
+
+import java.time.LocalDate;
+
+public class BoringTask extends Task{
+
+	public BoringTask() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BoringTask(String name, LocalDate dueDate) {
+		super(name, dueDate);
+	}
+	
+	 public void procratinate() {
+		 this.setDueDate(this.getDueDate().plusDays(1));
+	 }
+
+	
+	
+}
