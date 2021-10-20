1615e9ebff9a4741d20c2d44733a496ba0bcadff
pkorrie <patiencekorrie@gmail.com>
2021-10-20 17:04:42 -0400

    Update codeExplanation.java

--- week-2/codeExplanation.java
+++ week-2/codeExplanation.java
@@ -8,7 +8,7 @@ if (score == 80) { // An if statement checking if int score is equal to 80
 	result = "You passed!"; // if the above statement is true result is assigned the string "you passed!"
 } else if (score > 80) { // else if the value stored in the variable store is greather than 80
 	result = "You did great!";// result will be assigned the string "you did great!"
-} else { // else meaning if the both of the above statements are false
+} else { // else meaning if both of the above statements are false
 	result = "You did not pass. Try again!";// result will be assigned the string "you did not pass. Try again!"
 }
 
